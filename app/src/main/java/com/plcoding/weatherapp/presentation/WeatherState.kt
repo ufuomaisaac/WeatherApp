5fd965e49ad34f2ef3c4426a198f4a69e7b559be
ufuomaisaac <ejemudaroufuoma@gmail.com>
2024-03-22 20:55:07 +0100

    bugged

---
+++ app/src/main/java/com/plcoding/weatherapp/presentation/WeatherState.kt
@@ -0,0 +1,10 @@
+package com.plcoding.weatherapp.presentation
+
+
+import com.plcoding.weatherapp.domain.weather.WeatherInfo
+
+data class WeatherState(
+    val weatherInfo: WeatherInfo? = null,
+    val isLoading: Boolean = false,
+    val error: String? = null
+)
